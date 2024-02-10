5ff878fc3df63109e3374ce464df8a4168429736
Sean <seancyrill@gmail.com>
2024-02-10 09:11:45 +0800

    cors fix 4

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,11 +1,14 @@
 import cors from "cors";
 import "dotenv";
 
-const whitelist = [process.env.CLIENT_URL];
+const whitelist = process.env.CLIENT_URL;
 
 export const corsOptions: cors.CorsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
+    if (
+      origin === whitelist
+      //whitelist.indexOf(origin) !== -1 || !origin
+    ) {
       callback(null, true);
     } else {
       console.log(process.env.CLIENT_URL);
