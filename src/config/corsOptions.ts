d5dbde6254956ba577c3bde01d98d185fbd64f3a
Sean <seancyrill@gmail.com>
2024-04-19 02:47:30 +0800

    fix 1

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -5,6 +5,7 @@ const whitelist = process.env.CLIENT_URL;
 
 export const corsOptions: cors.CorsOptions = {
   origin: function (origin, callback) {
+    console.log({ origin, whitelist, result: origin === whitelist });
     if (
       origin.startsWith(whitelist)
       //whitelist.indexOf(origin) !== -1 || !origin
